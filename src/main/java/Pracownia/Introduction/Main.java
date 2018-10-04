ff2efe4615bdbdcd691820f661594cb818734508
s440563 <magdalena.curzi@gmail.com>
2018-10-04 13:02:24 +0200

    commit message

--- src/main/java/Pracownia/Introduction/Main.java
+++ src/main/java/Pracownia/Introduction/Main.java
@@ -1,8 +1,17 @@
 package Pracownia.Introduction;
 
+import org.apache.log4j.Logger;
+
 public class Main {
 
+    static Logger log = Logger.getLogger("abc"); //static method on class Logger, static object
+
+
     public static void main(String [ ] args) {
+
+        log.info("message");
+
+
         System.out.println("Hello World");
     }
 }
